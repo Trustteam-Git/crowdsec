4d860470b47e509652fef862469bf29bad0f4f21
Smoetzak <anthony.seys@outlook.com>
2022-05-10 13:28:29 +0200

    Updated list

--- containers/crowdsec/scripts/git_push.sh
+++ containers/crowdsec/scripts/git_push.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-echo "git add"
 git add -A
-echo "git status"
-git status  
-echo "git commit -a"       
+git status       
 git commit -m "Updated list"
-echo "git push"
 git push 
-echo "pause 5mins"

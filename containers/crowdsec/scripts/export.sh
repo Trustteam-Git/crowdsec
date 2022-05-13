1c8a691165e2260aff601a328d349543d4c8c715
Smoetzak <anthony.seys@outlook.com>
2022-05-13 13:51:23 +0200

    Updated list

--- containers/crowdsec/scripts/export.sh
+++ containers/crowdsec/scripts/export.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s -H "X-Api-Key: 32daafbb67bb690f03341cdebd9b7b5e" http://crowdsec:8080/v1/decisions/stream?startup=true  | jq '.new[].value' | sed 's/"//g' > /mnt/output/list.txt
+curl -s -H "X-Api-Key: ae5008d7f011231dfc7dc01760f22b55" http://crowdsec:8080/v1/decisions/stream?startup=true  | jq '.new[].value' | sed 's/"//g' > /mnt/output/list.txt
